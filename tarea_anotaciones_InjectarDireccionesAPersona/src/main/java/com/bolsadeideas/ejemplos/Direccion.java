32b376866ea9555ddcb225b7b69f15be87b9086a
alberto <alberto@Asus>
2016-01-16 19:01:10 +0100

    Terminamos la tarea 2

--- tarea_anotaciones_InjectarDireccionesAPersona/src/main/java/com/bolsadeideas/ejemplos/Direccion.java
+++ tarea_anotaciones_InjectarDireccionesAPersona/src/main/java/com/bolsadeideas/ejemplos/Direccion.java
@@ -1,8 +1,5 @@
 package com.bolsadeideas.ejemplos;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Direccion {
 	
 	private int numero = 111;
--- tarea_anotaciones_InjectarDireccionesAPersona/src/main/java/com/bolsadeideas/ejemplos/MisDirecciones.java
+++ tarea_anotaciones_InjectarDireccionesAPersona/src/main/java/com/bolsadeideas/ejemplos/MisDirecciones.java
@@ -1,30 +1,22 @@
 package com.bolsadeideas.ejemplos;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MisDirecciones implements IDirecciones {
 
-	private List<Direccion> listaDirecciones;
-
 	@Autowired
-	private void setDireccionesTarea(@Qualifier("primeraDireccion") Direccion primeraDireccion,
-			@Qualifier("segundaDireccion") Direccion segundaDireccion) {
-		this.listaDirecciones = new ArrayList<Direccion>();
-		this.listaDirecciones.add(primeraDireccion);
-		this.listaDirecciones.add(segundaDireccion);
-	}
+	private List<Direccion> listaDirecciones;
 
 	@Override
 	public List<Direccion> getListaDirecciones() {
 		return listaDirecciones;
 	}
 
+	
 	@Override
 	public void setListaDirecciones(List<Direccion> listaDirecciones) {
 		this.listaDirecciones = listaDirecciones;
